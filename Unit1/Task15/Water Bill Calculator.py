1ac9cc279c41ffb093f49f1fe4e52e9036c80684
Kusuma Vallabhaneni <kusumavallabhaneni64@gmail.com>
2026-06-18 16:07:49 +0530

    water Bill Calculator Task15

---
+++ Unit1/Task15/Water Bill Calculator.py
@@ -0,0 +1,4 @@
+water_consumption = float(input("Enter Water Consumption: ")) 
+rate = float(input("Enter Rate Per Unit: "))
+water_bill = water_consumption * rate 
+print("Water Bill =", water_bill)
