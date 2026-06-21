133a5a8826f1f89f40d953dab1a211b894353671
Kusuma Vallabhaneni <kusumavallabhaneni64@gmail.com>
2026-06-21 19:59:42 +0530

    Task10 deleted

--- Unit1/Task10/Percentage Calculator.py
+++
@@ -1,6 +0,0 @@
-total_marks = float(input("Enter Total Marks Obtained: "))
-max_marks = float(input("Enter Maximum Marks: "))
-
-percentage = (total_marks / max_marks) * 100
-
-print("Percentage =", percentage)
