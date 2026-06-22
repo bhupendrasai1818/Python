aef257e7080159effdf27a91be7c1b856af6f164
Bhuvana <rayalabhuvana7@gmail.com>
2026-06-22 08:50:45 +0530

    Budget Recommendation System

---
+++ Unit1/Task57/Budget Recommendation System.py
@@ -0,0 +1,13 @@
+monthly_income = float(input("Enter Monthly Income: "))
+savings = monthly_income * 0.20 
+food = monthly_income * 0.30
+transportation = monthly_income * 0.20
+entertainment = monthly_income * 0.10
+other_expenses = monthly_income * 0.20
+print("\n--- Budget Recommendation Report ---")
+print("Monthly Income:", monthly_income)
+print("Savings Allocation:", savings)
+print("Food Allocation:", food)
+print("Transportation Allocation:", transportation)
+print("Entertainment Allocation:", entertainment) 
+print("Other Expenses Allocation:", other_expenses)
